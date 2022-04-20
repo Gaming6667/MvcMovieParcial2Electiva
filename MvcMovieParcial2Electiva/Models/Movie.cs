b30deb028567cbe8276b93a667349f9ea050398a
Andres santamaria <anakin_aries@hotmail.com>
2022-04-19 23:43:31 -0500

    1Push - Añadir modelo

---
+++ MvcMovieParcial2Electiva/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovieParcial2Electiva.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public String? Genre { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
